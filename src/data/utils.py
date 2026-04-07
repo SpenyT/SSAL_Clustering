d0db37a5ed21d372ddc4d73822708ee553331206
SpenyT <spencer.toupin@outlook.com>
2026-04-07 16:29:05 -0400

    feat: added mean and std calculation

---
+++ src/data/utils.py
@@ -0,0 +1,72 @@
+import os
+from pyparsing import Dict
+import torch
+import pickle
+import torchvision
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+CIFAR_DIR = os.path.join(DATA_DIR, "cifar-100-python")
+VARIABLES_PATH = os.path.join(DATA_DIR, "variables.pkl")
+
+def calculate_mean_std(loader):
+    n = 0
+    s1 = torch.zeros(3)
+    s2 = torch.zeros(3)
+    for x, _ in loader:
+        B, C, H, W = x.shape
+        n += B * H * W
+        s1 += x.sum(dim=(0,2,3))
+        s2 += (x*x).sum(dim=(0,2,3))
+    mean = s1 / n
+    std = (s2 / n - mean**2).sqrt()
+    return mean.tolist(), std.tolist()
+
+
+def load_data(data_name=None):
+    if not os.path.exists(VARIABLES_PATH):
+        return {}
+    try:
+        with open(VARIABLES_PATH, 'rb') as f:
+            all_data = pickle.load(f)
+    except (EOFError, pickle.UnpicklingError):
+        return {}
+        
+    if data_name is None:
+        return all_data
+    if isinstance(data_name,str):
+        return all_data.get(data_name)
+    return [all_data.get(name) for name in data_name]
+
+
+def save_data(new_data):
+    all_data = load_data()
+    new_dict = all_data | new_data
+
+    with open(VARIABLES_PATH, 'wb') as f:
+        pickle.dump(new_dict, f)
+
+
+def calculate_save_mean_std():
+    stats_transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+
+    stats_dataset = torchvision.datasets.CIFAR100(
+        root=DATA_DIR,
+        train=True,
+        download=not os.path.exists(CIFAR_DIR),
+        transform=stats_transform
+    )
+
+    loader = DataLoader(
+        stats_dataset,
+        batch_size=256,
+        shuffle=False
+    )
+
+    mean, std = calculate_mean_std(loader)
+    save_data({"mean": mean, "std": std})
