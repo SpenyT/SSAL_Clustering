e2efd7893a494ae16ab94f16dac161f3ddef3d39
SpenyT <spencer.toupin@outlook.com>
2026-04-16 13:38:41 -0400

    fixed BASE_DIR

--- src/glob_config.py
+++ src/glob_config.py
@@ -6,7 +6,7 @@ from typing_extensions import Final
 SEED: Final[int] = 42
 
 # data paths config
-BASE_DIR:       Final[str] = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR:       Final[str] = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR:       Final[str] = f"{BASE_DIR}/data"
 CIFAR_DIR:      Final[str] = f"{DATA_DIR}/cifar-100-python"
 VARIABLES_PATH: Final[str] = f"{DATA_DIR}/variables.pkl"
